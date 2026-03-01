ca08d4db33f8710b121bcbeb2889fca6faab18f2
Piyush Badgujar <pgbadgujar007@gmail.com>
2026-03-01 16:17:45 +0530

    Refactor yml and app for url changes

--- backend/app.js
+++ backend/app.js
@@ -17,10 +17,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
-      "https://fullstack-ci.vercel.app",
-      "https://fullstack-user-crud.vercel.app",
-      "https://frontend-ruby-eight-82.vercel.app",
-      "https://frontend-alpha-five-45.vercel.app"
+      "https://fullstack-user-crud-ci.vercel.app"
     ],
     credentials: true,
   })
